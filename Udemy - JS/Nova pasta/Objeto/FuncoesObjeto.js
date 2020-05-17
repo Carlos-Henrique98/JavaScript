be036dbbf227f3623d85458cdca34c817c6b8669
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-05-16 23:47:47 -0300

    JS

--- Udemy - JS/Nova pasta/Objeto/FuncoesObjeto.js
+++ Udemy - JS/Nova pasta/Objeto/FuncoesObjeto.js
@@ -11,3 +11,23 @@ console.log(Object.entries(pessoa))
 Object.entries(pessoa).forEach(([chave,valor]) => {
     console.log(`${chave}: ${valor}`)
 });
+
+Object.defineProperty(pessoa, 'dataNascimento',{
+    enumerable : true,
+    writable : false,
+    value : '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+const desc = { a : 1}
+const o1 = {b : 2}
+const o2 = { c : 3, a: 4}
+const obj = Object.assign(desc,o1, o2)
+console.log(obj)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
