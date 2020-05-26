4ab668a4363e2bc08189f26d527d14eaa394e552
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-05-25 23:19:02 -0300

    Map's

---
+++ Udemy - JS/Nova pasta/Array/Map1.js
@@ -0,0 +1,14 @@
+const nums = [1, 2, 3, 4, 5]
+console.log(nums)
+let resultado = nums.map(function(e){
+    return e * 2
+})
+
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
+
+resultado = nums.map(triplo).map(soma10).map(paraDinheiro)
+console.log(resultado)
